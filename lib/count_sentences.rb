6255fe314ee863beefd38ff0b5d61fe410d477a6
github username <github email address>
2018-11-23 17:02:29 -0500

    OO Counting Sentences

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+    #my way:
+    # if self.end_with?(".")
+    #   return true
+    # else
+    #   return false
+    # end
+
+    #better way:
+    return self.end_with?(".")
   end
 
   def question?
-
+    return self.end_with?("?")
   end
 
   def exclamation?
-
+    return self.end_with?("!")
   end
 
-  def count_sentences
-
+  def count_sentences()
+    self.split(/[.?!]/).delete_if { |string| string == ""}.count
   end
 end
