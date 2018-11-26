918426b4e83220dc9418d8e5c5e74a0fa273e062
Benjamin Addai <addai.5@buckeyemail.osu.edu>
2018-11-26 14:22:35 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] =="."
   end
 
   def question?
-
+  self[-1] == "?"
   end
 
   def exclamation?
-
+  self[-1] == "!"
   end
 
   def count_sentences
-
+    count = 0
+  arr = self.split(/[.!?]/)
+  arr.each do |ele|
+    if ele != ""
+    count+=1
+    end
+  end
+    return count
   end
 end
+
+# "This, well, is a sentence. This is too!! And so is this, I think? Woo..."
